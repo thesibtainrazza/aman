70b86b31d3be4c23ee2983c228bd627b88dacaa5
thesibtainrazza <thesibtainrazza@gmail.com>
2025-06-19 10:21:59 +0530

    updates socials

--- src/constants/index.js
+++ src/constants/index.js
@@ -73,18 +73,22 @@ const socialImgs = [
   {
     name: "insta",
     imgPath: "/images/insta.png",
+    url:"https://www.instagram.com/harshsingh_s19/",
   },
   {
     name: "fb",
     imgPath: "/images/fb.png",
+    url:"https://www.instagram.com/harshsingh_s19/",
   },
   {
     name: "x",
     imgPath: "/images/x.png",
+    url:"https://x.com/Asengar88568",
   },
   {
     name: "linkedin",
     imgPath: "/images/linkedin.png",
+    url:"https://www.linkedin.com/in/aman-singh-sengar",
   },
 ];
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,13 +7,21 @@ const Footer = () => {
         <div className="flex flex-col justify-center">
           <p>Terms & Conditions</p>
         </div>
-        <div className="socials">
+
+        <div className="socials flex gap-4">
           {socialImgs.map((socialImg, index) => (
-            <div key={index} className="icon">
-              <img src={socialImg.imgPath} alt="social icon" />
-            </div>
+            <a
+              key={index}
+              href={socialImg.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="icon"
+            >
+              <img src={socialImg.imgPath} alt={`${socialImg.name} icon`} />
+            </a>
           ))}
         </div>
+
         <div className="flex flex-col justify-center">
           <p className="text-center md:text-end">
             © {new Date().getFullYear()} Aman Singh. All rights reserved.
